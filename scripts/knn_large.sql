4e62585706d8a8e300ca50fe060ac82790b86466
dkakkar <devikakakkar29@gmail.com>
2019-05-24 15:57:05 -0400

    Update knn_large.sql

--- scripts/knn_large.sql
+++ scripts/knn_large.sql
@@ -8,8 +8,9 @@ Create index NY5_gix on NY5 using gist(geom);
 
 #Create index NY5_id on NY5(id);
 
-Create index geohash_gix on NY5(geohash);
-CLUSTER NY5 USING geohash-gix;
+Create index NY5_geohash on NY5(geohash);
+
+CLUSTER NY5 USING NY5_geohash;
 
 Create table knn_1000_NY5 (source_id character varying(255), neighbor_id character varying(255), neighbor_pid character varying(255),dist float);
 
