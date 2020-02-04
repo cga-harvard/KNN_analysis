517cac4e76d09ae313092e1803255af65d16e7c7
dkakkar <devikakakkar29@gmail.com>
2020-02-04 12:21:48 -0500

    Update knn_large.sql

--- scripts/knn_large.sql
+++ scripts/knn_large.sql
@@ -19,37 +19,34 @@ pg_dump -h localhost -p 7584 -Fc partisandb > /n/scratchlfs02/cga/dkakkar/partis
                                                                                                                                      
 ### KNN cacluation begin here
 
-### Create table NY5 as (select id,pid,geom,grp from us_voters_grp where grp= 'NY5');
+Create table RI as (select id,geom,grp from partisan where grp= 'RI');
 
-Create table NY5 as (select id,pid,geom,grp,geohash from us_voters_grp where grp= 'NY5');
 
-Alter table NY5 Add column row_id Serial primary key;
+Alter table RI Add column row_id Serial primary key;
 
-Create index NY5_gix on NY5 using gist(geom);
+Create index RI_gix onRI using gist(geom);
 
-#Create index NY5_id on NY5(id);
+CREATE INDEX RI ON nyc_census_blocks (ST_GeoHash(ST_Transform(geom,4326)));
 
-CREATE INDEX NYC ON nyc_census_blocks (ST_GeoHash(ST_Transform(geom,4326)));
+CLUSTER RI USING NY5_geohash;
 
-CLUSTER NY5 USING NY5_geohash;
-
-Create table knn_1000_NY5 (source_id character varying(255), neighbor_id character varying(255), neighbor_pid character varying(255),dist float);
+Create table knn_1000_RI (source_id character varying(255), neighbor_id character varying(255),dist float);
 
 
 DO $$
 BEGIN
-FOR counter IN 1..(Select count(*) from NY5)
+FOR counter IN 1..(Select count(*) from RI)
 LOOP
-INSERT INTO knn_1000_NY5(
-SELECT a.id as source_id, b.id as neighbor_id, b.pid as neighbor_pid,ST_DistanceSphere((SELECT geom FROM NY5 WHERE row_id = counter), b.geom) AS dist
-FROM NY5 a, us_voters_grp b
+INSERT INTO knn_1000_RI(
+SELECT a.id as source_id, b.id as neighbor_id, ST_DistanceSphere((SELECT geom FROM RI WHERE row_id = counter), b.geom) AS dist
+FROM RI a, us_voters_grp b
 WHERE a.id <> b.id
 AND a.row_id = counter
-ORDER BY (SELECT geom FROM NY5 WHERE row_id = counter) <-> b.geom
+ORDER BY (SELECT geom FROM RI WHERE row_id = counter) <-> b.geom
 LIMIT 1000)
 ;
 END LOOP;
 END; $$
 ;
 
-Drop table NY5; 
+Drop table RI; 
